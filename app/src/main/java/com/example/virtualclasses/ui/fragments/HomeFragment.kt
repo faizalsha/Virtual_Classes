cb455244b08b7bcb5a3ccc41fdfde7cc8b51787d
shada <shadabfaizal7@gmail.com>
2020-11-08 22:03:59 +0530

    changed home layout

--- app/src/main/java/com/example/virtualclasses/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/virtualclasses/ui/fragments/HomeFragment.kt
@@ -2,15 +2,14 @@ package com.example.virtualclasses.ui.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.virtualclasses.R
-import com.example.virtualclasses.firebase.FireAuth
 import com.example.virtualclasses.firebase.FireStore
 import com.example.virtualclasses.local.Constants
 import com.example.virtualclasses.local.SharedPrefsUtils
